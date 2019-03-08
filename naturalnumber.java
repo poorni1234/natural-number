bdfc9eaa804bc3733c8945caac291bf572f870e9
poorni1234 <47376824+poorni1234@users.noreply.github.com>
2019-03-08 01:07:17 -0800

    Create naturalnumber.java

---
+++ naturalnumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Natural
+  {
+	public static void main(String args[])
+	   {
+	  int n=0;
+		Scanner nc=new Scanner(System.in);
+		int n=nc.nextInt();
+		for(int i=1;i<=n;i++)
+		n=n+i;
+		System.out.println(n);
+    }
+}
+	
+
